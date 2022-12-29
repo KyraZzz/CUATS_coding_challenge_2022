294d822b685b2bdbc26759c4e024610801d9605d
Kyra Zhou <71659144+KyraZzz@users.noreply.github.com>
2022-12-29 14:28:29 +0000

    Create long_term_pair_trading.py

---
+++ long_term_pair_trading.py
@@ -0,0 +1,39 @@
+# region imports
+from AlgorithmImports import *
+# endregion
+
+class AlgoTradingCourse10(QCAlgorithm):
+    """ Simple long-term strategy: SPY (equity) and BND (bond) usually have a negative correlation
+        Uptrend then do 80% SPY and 20% BND, downtrend then do 20% SPY and 80% BND
+        Rebalance to 80% SPY and 20% BND if no trand change for 30 days
+    """
+    def Initialize(self):
+        self.SetStartDate(2018, 1, 1)  # Set Start Date
+        self.SetEndDate(2021, 1, 1)
+        self.SetCash(100000)  # Set Strategy Cash
+        self.spy = self.AddEquity("SPY", Resolution.Daily).Symbol
+        self.bnd = self.AddEquity("BND", Resolution.Daily).Symbol
+
+        length = self.GetParameter("sma_length")
+        length = 30 if length is None else int(length)
+        self.sma = self.SMA(self.spy, length, Resolution.Daily)
+        self.rebalanceTime = datetime.min
+        self.uptrend = True
+
+    def OnData(self, data: Slice):
+        if not self.sma.IsReady or self.spy not in data or self.bnd not in data:
+            return
+        
+        if data[self.spy].Price >= self.sma.Current.Value:
+            if self.Time >= self.rebalanceTime or not self.uptrend:
+                self.SetHoldings(self.spy, 0.8)
+                self.SetHoldings(self.bnd, 0.2)
+                self.uptrend = True
+                self.rebalanceTime = self.Time + timedelta(30)
+        elif self.Time >= self.rebalanceTime or self.uptrend:
+            self.SetHoldings(self.spy, 0.2)
+            self.SetHoldings(self.bnd, 0.8)
+            self.uptrend = False
+            self.rebalanceTime = self.Time + timedelta(30)
+        
+        self.Plot("Benchmark", "SMA", self.sma.Current.Value)
